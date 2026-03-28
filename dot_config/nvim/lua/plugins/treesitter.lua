a80109bfb815d06db189d1f7508144aa6df9b158
Alexander Anderson <revolution6000@gmail.com>
2026-03-28 12:44:35 +0000

    Initial commit

---
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
+    build = ":TSUpdate",
+
+    config = function()
+	local configs = require("nvim-treesitter.configs")
+	configs.setup({
+	    highlight = { enable = true },
+	    indent = { enable = true },
+	    ensure_installed = {
+		"lua",
+		"tsx",
+		"html",
+	    },
+	    auto_install = false,
+	})
+    end
+}
