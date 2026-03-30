4e0d234a83338cfcc3c3c777b88b191455551c10
Alexander Anderson <revolution6000@gmail.com>
2026-03-30 22:02:16 +0100

    Catppuccin stuff

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,10 @@
-require('config.options');
-require('config.keybinds');
-require('config.lazy');
+require("jonk_vim")
+local cat = "catppuccin-mocha";
+vim.cmd("colorscheme catppuccin-mocha")
+
+require("lualine").setup({
+    options = {
+        icons_enabled = true,
+        theme = cat,
+    }
+})
